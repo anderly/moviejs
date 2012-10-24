0e9d143ea0eaacac2ac9df26465ed1e64cf2abac
Adam Anderly <adam@anderly.com>
2012-10-24 12:43:42 -0500

    Fix for title

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-	"connectionString" : process.env.connectionString
+	"connectionString" : process.env.connectionString || "redis://redistogo:11057819af4dd82422458a5fb4803b4b@koi.redistogo.com:9484/"
 }
